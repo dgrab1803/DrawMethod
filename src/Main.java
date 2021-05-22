e2260c47a329289bc7e100c1a5fd77220f651846
D.Grabovski <58883678+dgrab1803@users.noreply.github.com>
2021-05-22 16:17:35 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MethodDemo appwin = new MethodDemo();
+
+        appwin.setSize(new Dimension(370, 700));
+        appwin.setTitle("GraphicsDemo");
+        appwin.setVisible(true);
+    }
+}
